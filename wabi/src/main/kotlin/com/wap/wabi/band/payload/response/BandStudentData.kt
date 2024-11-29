d742a09229741f574ca618514d14824754f2b45d
FhRh <yiyop@naver.com>
2024-11-29 18:34:59 +0900

    fix(JoinDate) : 가입일자 없어도 조회 가능

--- wabi/src/main/kotlin/com/wap/wabi/band/payload/response/BandStudentData.kt
+++ wabi/src/main/kotlin/com/wap/wabi/band/payload/response/BandStudentData.kt
@@ -1,14 +1,13 @@
 package com.wap.wabi.band.payload.response
 
 import com.wap.wabi.band.entity.BandStudent
-import java.time.LocalDate
 
 data class BandStudentData(
     val studentId: String,
     val name: String,
     val club: String,
     val position: String,
-    val joinDate: LocalDate,
+    val joinDate: String,
     val college: String,
     val major: String,
     val tel: String,
@@ -27,7 +26,7 @@ data class BandStudentData(
                 name = bandStudent.student.name,
                 club = bandStudent.club,
                 position = bandStudent.position,
-                joinDate = bandStudent.joinDate,
+                joinDate = bandStudent.joinDate?.toString() ?: "",
                 college = bandStudent.college,
                 major = bandStudent.college,
                 tel = bandStudent.tel,
